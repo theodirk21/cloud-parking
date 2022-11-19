9c9b08a66efe8021c752f0cf201779ce2660f178
theodirk21 <theodirk17@gmail.com>
2022-11-19 06:59:54 -0300

    atualização

--- src/main/java/one/digitalinnovation/cloudparking/controller/HelloController.java
+++ src/main/java/one/digitalinnovation/cloudparking/controller/HelloController.java
@@ -1,14 +1,15 @@
 package one.digitalinnovation.cloudparking.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class HelloController {
 
     @GetMapping
     public String hello(){
-        return "Hello Dio! Java dev";
+        return "Hello DIO Java Devs";
     }
-
 }

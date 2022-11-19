4347d9ee98698b8c619d67e217d55ac6c8ab14ce
theodirk21 <theodirk17@gmail.com>
2022-11-19 08:12:58 -0300

    atualizações

--- src/main/java/one/digitalinnovation/cloudparking/controller/ParkingController.java
+++ src/main/java/one/digitalinnovation/cloudparking/controller/ParkingController.java
@@ -4,7 +4,9 @@ import one.digitalinnovation.cloudparking.controller.dto.ParkingDTO;
 import one.digitalinnovation.cloudparking.controller.mapper.ParkingMapper;
 import one.digitalinnovation.cloudparking.model.Parking;
 import one.digitalinnovation.cloudparking.service.ParkingService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,9 +25,16 @@ public class ParkingController {
     }
 
     @GetMapping
-    public List<ParkingDTO> findAll(){
+    public ResponseEntity<List<ParkingDTO>> findAll(){
         List<Parking> parkingList = parkingService.findAll();
         List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
-        return result;
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ParkingDTO> findById(@PathVariable String id){
+        Parking parking = parkingService.findById(id);
+        ParkingDTO result = parkingMapper.toParkingDTO(parking);
+        return ResponseEntity.ok(result);
     }
 }
--- src/main/java/one/digitalinnovation/cloudparking/service/ParkingService.java
+++ src/main/java/one/digitalinnovation/cloudparking/service/ParkingService.java
@@ -16,8 +16,11 @@ public class ParkingService {
     
     static {
         var id = getUUID();
+        var id2 = getUUID();
         Parking parking = new Parking(id, "hms-908", "SP", "Mercedes", "Branco");
+        Parking parking2 = new Parking(id2, "jgs-908", "RJ", "BMW", "Vermelho");
         parkingMap.put(id, parking);
+        parkingMap.put(id2, parking2);
     }
 
     public List<Parking> findAll(){
@@ -26,5 +29,9 @@ public class ParkingService {
 
     private static String getUUID() {
         return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    public Parking findById(String id) {
+        return parkingMap.get(id);
     }
 }
